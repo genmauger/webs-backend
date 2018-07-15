d4b94a998c53bc3cf54ceef5c9dff32a2db12100
SVETO <svetostoj@hotmail.com>
2018-07-15 20:35:03 +1000

    Booking new and remove

--- workshops/workshopRouter.js
+++ workshops/workshopRouter.js
@@ -2,7 +2,7 @@ const express = require('express')
 const Workshop = require('./workshop')
 const router = express.Router()
 
-// all our workshops
+
 router.get('/', (req, res) => {
     Workshop.find()
         .then(workshops => {
@@ -13,5 +13,48 @@ router.get('/', (req, res) => {
         })
 })
 
+
+router.post('/', (req, res) => {
+
+    const workshop = new Workshop(req.body)
+    workshop.save()
+        .then(() => {
+            res.status(201).json(workshop)
+        })
+        .catch(err => {
+            res.status(500).json({ err: err.message })
+        })
+})
+
+
+router.put('/:id/new', function (req, res, next) {
+    Workshop.findOne({ _id: req.params.id }, function (err, workshop) {
+        if (err) return next(err);
+
+        workshop.bookings.push(req.body)
+
+        workshop.save()
+        return res.json(workshop)
+    })
+
+});
+
+router.put('/:id/:id_sub/remove', function (req, res, next) {
+    Workshop.findOne({ _id: req.params.id }, function (err, workshop) {
+        if (err) return next(err);
+
+
+        for (let i = 0; i < workshop.bookings.length; i++) {
+            // console.log(workshop.bookings[i]._id)
+            if (workshop.bookings[i]._id == req.params.id_sub) {
+                var ind = i
+            }
+        }
+        workshop.bookings[ind].remove()
+        workshop.save()
+        return res.json(workshop)
+    })
+
+});
 
 module.exports = router
