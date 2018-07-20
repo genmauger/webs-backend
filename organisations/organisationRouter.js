bf0fecb14fa0c1dfdd04889dfb70a85a25349690
Steven Christenson <stevenchristenson@Stevens-MacBook-Air.local>
2018-07-20 18:42:16 +1000

    Organisation routes updated

--- organisations/organisationRouter.js
+++ organisations/organisationRouter.js
@@ -4,34 +4,53 @@ const router = express.Router()
 
 
 router.get('/', (req, res) => {
-    Organisation.find()
-        .then(organisations => {
-            res.status(200).json(organisations)
-        })
-        .catch(err => {
-            res.status(500).json({ error: err.message })
-        })
+ Organisation.find()
+   .then(organisations => {
+     res.status(200).json(organisations)
+   })
+   .catch(err => {
+     res.status(500).json({ error: err.message })
+   })
+});
+
+router.get('/:id', (req, res) => {
+  Organisation.findById({ _id: id }, req.body)
 })
 
-router.patch("/:id", (req, res, next) => {
-  const id = req.params.id
+router.delete('/:id', (req, res) => {
+ const id = req.params.id
+ Organisation.findByIdAndRemove(id, function (err, organisation) {
+     if (err) {
+         throw err;
+     } else {
+         res.status(204).json({
+             deleted: true
+         })
+     }
+ })
+});
 
-  Organisation.findByIdAndUpdate({_id: id}, req.body,{new: true}, function(err, organisation){
-    if(err) return res.status(500).send(err);
-    return res.body.json(organisation);
-  })
-})
+router.patch('/:id', (req, res, next) => {
+ // const id = req.params.productId;
+ const id = req.params.id
+ 
+ Organisation.findByIdAndUpdate({ _id: id },  req.body, {new: true}, function(err, organisation) {
+     if (err) return res.status(500).send(err);
+     return res.json(organisation);
+ })
+});
 
-router.post ('/new', (req, res) => {
 
-  const event = new Organisation(req.body)
-  event.save()
-       .then(() => {
-            res.status(201).json(organisation)
-       })
-       .catch(err => {
-         res.status(500).json({err: err.message})
-       })
-})
+router.post('/new', (req, res) => {
+
+ const organisation = new Organisation(req.body)
+ organisation.save()
+     .then(() => {
+         res.status(201).json(organisation)
+     })
+     .catch(err => {
+         res.status(500).json({ err: err.message })
+     })
+});
 
 module.exports = router
